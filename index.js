b5566d1340ab24f30d4eabd392c0ade4c76134af
niuyanwen <975279981@qq.com>
2023-02-23 15:54:50 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const app = express()
+router.post('/message/post', async ctx => {
+  const { ToUserName, FromUserName, Content, CreateTime } = ctx.request.body;
 
-app.get('/', (req, res) => {
-  res.send('欢迎使用微信云托管！')
-})
-
-const port = process.env.PORT || 80
-app.listen(port, () => {
-  console.log('服务启动成功，端口：', port)
-})
+  ctx.body = {
+    ToUserName: FromUserName,
+    FromUserName: ToUserName,
+    CreateTime: +new Date(),
+    MsgType: 'text',
+    Content: `反弹你发的消息：${Content}`,
+  };
+});
